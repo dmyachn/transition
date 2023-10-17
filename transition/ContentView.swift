a58bb5cb326dcfcbfe8e3e65bbd75e55aa28f04b
Dmitry <d.myachn@gmail.com>
2023-10-17 17:18:04 +0300

    done

--- transition/ContentView.swift
+++ transition/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Namespace private var animation
+    @State private var isOpen = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Spacer()
+                .overlay(alignment: isOpen ? .center : .bottomTrailing) {
+                    if isOpen{
+                        RoundedRectangle(cornerSize: CGSize(width: 20, height: 20), style: .continuous)
+                            .foregroundColor(.blue)
+                            .matchedGeometryEffect(id: "Button", in: animation)
+                            .frame(width: 350,height: 520)
+                            .overlay(alignment: .topLeading) {
+                                Button(action: {
+                                    withAnimation{
+                                        isOpen.toggle()
+                                    }
+                                }, label: {
+                                    HStack{
+                                        Image(systemName: "arrow.down.right.topleft.rectangle")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 30)
+                                        Text("Close")
+                                            .frame(width: 70)
+                                            .font(.title2)
+                                            .bold()
+                                    }
+                                    .padding()
+                                    .matchedGeometryEffect(id: "Title", in: animation)
+                                    .foregroundStyle(.white)
+                                })
+                                .padding()
+                            }
+                    } else {
+                        Button(action: {
+                            withAnimation{
+                                isOpen.toggle()
+                            }
+                        }, label: {
+                            RoundedRectangle(cornerSize: CGSize(width: 20, height: 20), style: .continuous)
+                                .foregroundColor(.blue)
+                                .matchedGeometryEffect(id: "Button", in: animation)
+                                .frame(width: 120,height: 70)
+                                .overlay(alignment: .center) {
+                                    Text("Open")
+                                        .foregroundStyle(.white)
+                                        .font(.title2)
+                                        .bold()
+                                        .matchedGeometryEffect(id: "Title", in: animation)
+                                }
+                        })
+                        .padding()
+                    }
+                }
         }
-        .padding()
     }
 }
 
