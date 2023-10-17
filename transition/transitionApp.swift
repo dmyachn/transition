5fb35b2d5683bddc511c35bdf1dc6b4f350bac58
Dmitry <d.myachn@gmail.com>
2023-10-17 15:18:47 +0300

    Initial Commit

---
+++ transition/transitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  transitionApp.swift
+//  transition
+//
+//  Created by Dmitiy Myachin on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct transitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
