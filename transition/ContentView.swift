ac171585650ee1a3a97fe5e4934f3d743f9a79da
Dmitry <d.myachn@gmail.com>
2023-10-17 17:49:44 +0300

    fix text

--- transition/ContentView.swift
+++ transition/ContentView.swift
@@ -35,8 +35,8 @@ struct ContentView: View {
                                             .font(.title2)
                                             .bold()
                                     }
-                                    .padding()
                                     .matchedGeometryEffect(id: "Title", in: animation)
+                                    .padding()
                                     .foregroundStyle(.white)
                                 })
                                 .padding()
